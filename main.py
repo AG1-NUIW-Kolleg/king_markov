7a47b513ed498ec9264fa6755e9e656d3d6aed4b
Veit Gemmer <gemv@viessmann.com>
2024-03-19 09:27:41 +0100

    updated requirements, changed island name

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ from king_markov import KingMarkov
 from kingdom import Kingdom
 
 
-ITERATIONS = 1e7
+ITERATIONS = 1e8
 
-island_0 = Island(population_size=100)
-island_1 = Island(population_size=200)
-island_2 = Island(population_size=300)
-island_3 = Island(population_size=400)
-island_4 = Island(population_size=500)
-islands = [island_0, island_1, island_2, island_3, island_4]
+iceland = Island(population_size=100)
+new_zealand = Island(population_size=200)
+madagaskar = Island(population_size=300)
+great_britain = Island(population_size=400)
+australia = Island(population_size=500)
+islands = [iceland, new_zealand, madagaskar, great_britain, australia]
 bikini_bottum = Kingdom(islands)
 
 bikini_bottum.setup_island_order()
 
 markov = KingMarkov(kingdom=bikini_bottum, starting_island_id=0)
+
 for i in tqdm(
     range(0, int(ITERATIONS)),
     desc='King Markov is traveling', ncols=80,
